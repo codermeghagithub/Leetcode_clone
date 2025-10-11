a51507c06cfc56da0a7295c470efdd343bf5c2de
codermeghagithub <meghade2003@gmail.com>
2025-10-11 06:01:13 +0530

    first commit

---
+++ Backend/src/index.js
@@ -0,0 +1,20 @@
+import express from "express";
+import dotenv from "dotenv"
+
+dotenv.config()
+const app=express();
+
+
+const PORT=process.env.PORT || 8080
+app.get("/",(req,res)=>{
+res.json({
+  success:true,
+  message:"Welcome to our Leetcode Clone API"
+})
+})
+
+
+app.listen(PORT,()=>{
+  console.log(`your server is running on port http://localhost:${PORT}`);
+  
+})
